9ebb5862016d41147d0726b7551fd5d5ce6fdda2
hisakey <hogehoge@example.com>
2025-11-10 21:28:14 +0900

    feat: implement sidekiq ui

--- lab/rails/sandbox_api/config/application.rb
+++ lab/rails/sandbox_api/config/application.rb
@@ -29,6 +29,8 @@ module App
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore, key: "_cs_lab_session"
     config.active_job.queue_adapter = :sidekiq
   end
 end
---
+++ lab/rails/sandbox_api/config/initializers/session_store.rb
@@ -0,0 +1,1 @@
+Rails.application.config.session_store :cookie_store, key: "_cs_lab_session", same_site: :lax
