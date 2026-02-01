b81ebf1f52d339e22ef2fac82fc4e8138a5ecee8
hisakey <hogehoge@example.com>
2026-02-01 14:40:39 +0900

    feat: 83 delete_duplicates

---
+++ algo/ruby/leetcode/83.rb
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+class ListNode
+    attr_accessor :val, :next
+    def initialize(val = 0, _next = nil)
+        @val = val
+        @next = _next
+    end
+end
+# @param {ListNode} head
+# @return {ListNode}
+def delete_duplicates(head)
+  current = head
+  while current && current.next
+    if current.val == current.next.val
+      current.next = current.next.next
+    else
+      current = current.next
+    end
+  end
+
+  head
+end
